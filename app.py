e6fc99eb72ba995c87968a25a84b7ee9edd97ec5
David O. Neiva <113924063+DavidOliveiraNeiva@users.noreply.github.com>
2025-06-05 01:43:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template, redirect
 import pandas as pd
+import os
 from modelo import prever_gasto
 
 app = Flask(__name__)
@@ -22,3 +23,7 @@ def adicionar():
     df = pd.concat([df, pd.DataFrame([novo])])
     df.to_csv(DATA_FILE, index=False)
     return redirect('/')
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 10000))  # Render define a variável PORT
+    app.run(host='0.0.0.0', port=port)
